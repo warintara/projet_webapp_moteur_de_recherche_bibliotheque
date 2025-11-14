dee34f983b07bdd561df8b139f72604debf0b919
warintara <cartooneiei555@hotmail.com>
2025-11-14 14:40:32 +0100

    ajout les getters

--- mySearchEngine/mygutenberg/urls.py
+++ mySearchEngine/mygutenberg/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('books/', views.book_list),
+    path("books/", views.book_list),
+    path("book/<int:book_id>/", views.book_detail),
+    path("books/lang/<str:langage>/", views.book_langagage),
+    path("book/<int:book_id>/coverImage/", views.cover_image),
 ]
--- mySearchEngine/mygutenberg/views.py
+++ mySearchEngine/mygutenberg/views.py
@@ -2,11 +2,57 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import requests
+from django.urls import path
+from . import views
+
+BASE_URL = "https://gutendex.com/books"
 
 # Create your views here.
+
+# liste des livres (avec search & languages)
 @api_view(['GET'])
 def book_list(request):
-    url = "https://gutendex.com/books/?languages=fr"
+    search = request.GET.get("search", "")
+    language = request.GET.get("language", "")
+
+    params = []
+
+    if search:
+        params.append(f"search={search}")
+    if language:
+        params.append(f"languages={language}")
+
+    url = BASE_URL
+    if params:
+        url += "?" + "&".join(params)
+
     response = requests.get(url)
     return Response(response.json())
 
+# liste des livres par language
+@api_view(['GET'])
+def book_langagage(request, langage):
+    url = f"{BASE_URL}?language={langage}"
+    response = requests.get(url)
+    return Response(response.json())
+
+# détail d'un livre
+@api_view(['GET'])
+def book_detail(request, book_id):
+    url = f"{BASE_URL}/{book_id}"
+    response = requests.get(url)
+    return Response(response.json())
+
+# image de couverture d'un livre
+@api_view(['GET'])
+def cover_image(request, book_id):
+    url = f"{BASE_URL}/{book_id}"
+    response = requests.get(url)
+    data = response.json()
+
+    cover_url = data.get("formats", {}).get("image/jpeg", None)
+
+    if not cover_url:
+        return Response({"error": "No cover image available"}, status=404)
+
+    return Response({"cover_image_url": cover_url})
